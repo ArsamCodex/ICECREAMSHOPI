5a47bd27cedf12d866712aa45d53cc10bc787421
ArsamCodex <62879887+ArsamCodex@users.noreply.github.com>
2020-03-31 20:07:09 +0200

    V

--- src/be/intecbrussel/application/Stock.java
+++ src/be/intecbrussel/application/Stock.java
@@ -1,10 +1,10 @@
 package be.intecbrussel.application;
 
 public class Stock {
-    private int iceRockets = 100;
-    private int cones = 300;
-    private int balls = 1000;
-    private int magni = 50;
+    private int iceRockets ;
+    private int cones ;
+    private int balls ;
+    private int magni ;
 
     public Stock() {
         super();
