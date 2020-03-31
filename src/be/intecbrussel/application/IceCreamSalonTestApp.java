15e1a7b20ff4c980eaeae16ec909fbc5b141fe8c
ArsamCodex <62879887+ArsamCodex@users.noreply.github.com>
2020-03-31 19:40:25 +0200

    Update IceCreamSalonTestApp.java

--- src/be/intecbrussel/application/IceCreamSalonTestApp.java
+++ src/be/intecbrussel/application/IceCreamSalonTestApp.java
@@ -1,71 +1,57 @@
-package be.intecbrussel.application;
+ package be.intecbrussel.application;
 
 import be.intecbrussel.eatbles.Cone;
 import be.intecbrussel.eatbles.Eatable;
 import be.intecbrussel.eatbles.Magnum;
 import be.intecbrussel.seller.IceCreamCar;
+import be.intecbrussel.seller.IceCreamSalon;
 import be.intecbrussel.seller.IceCreamSeller;
 import be.intecbrussel.seller.PriceList;
 
 public class IceCreamSalonTestApp {
-    public static void main(String[] args) {
+ 
+
+public class forme {
+    public static void main (String [] args){
+
+        PriceList priceList = new PriceList(2.30,4.0,5.0);
+
+        IceCreamSalon iceCreamSeler = new IceCreamSalon();
+        orderMethod(iceCreamSeler);
 
 
-        iceCreamSalonPricelistConstructorTest();
     }
 
-    private static void iceCreamSalonPricelistConstructorTest() {
-        System.out.println("testing icreCreamCar with pricelist in constructor\n");
-        PriceList priceList = new PriceList(1.10,1.75,1.5);
-        Stock stock = new Stock(10, 10, 10, 10);
+    public static void orderMethod(IceCreamSeller iceCreamSeller){
+
+
+        PriceList priceList = new PriceList(4.10,6.90,1.50);
+        Stock stock = new Stock(40, 58, 70, 18);
         IceCreamSeller seller = new IceCreamCar(priceList,stock);
-			testOrderCone(seller);
-    		testOrderIceRocket(seller);
-			testOrderMagnum(seller);
-			testProfit(seller);
-        testOrderAThousandIceRockets(seller);
-    }
+        OrderCone(seller);
+        OrderIceRocket(seller);
+        OrderMagnum(seller);
+        Profit(seller);
 
-    private static void testOrderAThousandIceRockets(IceCreamSeller seller) {
-        Eatable[] rockets = new Eatable[1000];
-        for(int i = 0; i<1000;i++) {
-            rockets[i] = seller.orderIceRocket();
         }
-        System.out.println(rockets[0]);
-        System.out.println(rockets[999]);
 
-    }
-
-    private static void testProfit(IceCreamSeller seller) {
-        System.out.println("testing profit");
+    private static void Profit(IceCreamSeller seller) {
         System.out.println("profit is: " + seller.getProfit());
-        System.out.println("profit is larger than 0: " + (seller.getProfit()>0));
-
     }
 
-    private static void testOrderMagnum(IceCreamSeller seller) {
-        System.out.println("testing ther ordering of a magnum");
+    private static void OrderMagnum(IceCreamSeller seller) {
         Eatable magnum = seller.orderMagnum(Magnum.MagnumType.WHITECHOCOLATE);
         System.out.println(magnum);
-
     }
 
-    private static void testOrderIceRocket(IceCreamSeller seller) {
-        System.out.println("testing ther ordering of an IceRocket");
+    private static void OrderIceRocket(IceCreamSeller seller) {
         Eatable rocket = seller.orderIceRocket();
         System.out.println(rocket);
-
     }
 
-    private static void testOrderCone(IceCreamSeller seller) {
-        System.out.println("testing ther ordering of a cone");
+    private static void OrderCone(IceCreamSeller seller) {
         Eatable cone = seller.orderCone(new Cone.Flavor[]{Cone.Flavor.MOKKA});
         System.out.println(cone);
-
     }
-
-
-
-
 }
 
